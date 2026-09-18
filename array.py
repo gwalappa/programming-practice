c86d108b7aed82869c5d26507ebf0516c1808e0a
gwalappa <muttumali33@gmail.com>
2026-09-18 20:56:04 +0530

    practice

---
+++ array.py
@@ -0,0 +1,19 @@
+#NUMPY OPERATIONS
+import numpy as np
+a=np.array([1,2,3,4,5])
+b=np.array([6,7,8,9,10])
+
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+print(a**b)
+
+print(a.shape)
+print(a.ndim)
+print(a.size)
+
+print(b.size)
+print(b.shape)
+print(b.ndim)
+print()
